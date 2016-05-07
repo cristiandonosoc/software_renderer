886c983cff748f73fd5a1232ef649e4822095807
Cristián Donoso C <cristiandonosoc@gmail.com>
2016-05-06 22:16:29 -0300

    Bugfix for sometimes getting bounding box bigger than buffer

--- platform_independent/graphics/utils.c
+++ platform_independent/graphics/utils.c
@@ -19,6 +19,7 @@ void ClearBuffer(graphics_buffer *buffer, int color)
 // Borders are INCLUSIVE
 box2i GetTriangleBoundingBox2i(vec2i vertices[3], graphics_buffer *buffer)
 {
+    // Max impossible bounding box at start
     box2i box = { .minX = buffer->width - 1, 
                   .minY = buffer->height - 1,
                   .maxX = 0, 
@@ -26,11 +27,12 @@ box2i GetTriangleBoundingBox2i(vec2i vertices[3], graphics_buffer *buffer)
 
     for(int i = 0; i < 3; ++i)
     {
-        if (vertices[i].x < box.minX) { box.minX = vertices[i].x; }
-        if (vertices[i].x > box.maxX) { box.maxX = vertices[i].x; }
-        if (vertices[i].y < box.minY) { box.minY = vertices[i].y; }
-        if (vertices[i].y > box.maxY) { box.maxY = vertices[i].y; }
-
+        int x = vertices[i].x;
+        int y = vertices[i].y;
+        if ((x < box.minX) && (x >= 0)) { box.minX = x; }
+        if ((y < box.minY) && (y >= 0)) { box.minY = y; }
+        if ((x > box.maxX) && (x < buffer->width))  { box.maxX = x; }
+        if ((y > box.maxY) && (y < buffer->height)) { box.maxY = y; }
     }
     return box;
 }
